87cd17af2f768477263599f5a352c25322005a30
I.P <ilia.yavorov.petrov@gmail.com>
2024-02-27 22:57:06 +0200

    remove comment

--- exams/bash/2019-se-03/main.sh
+++ exams/bash/2019-se-03/main.sh
@@ -4,7 +4,6 @@ get_depth_file() {
     echo "$1" | tr -dc "/" | wc -c
 }
 
-#in_last="$(find ~ -maxdepth 5 -type f -print0 | xargs -0 stat --format "%i %n %Z")" | sort -t ' ' -k 3 -nr | head -n +1 | cut -d ' ' -f1
 in_last="$(find ~ -maxdepth 5 -type f -print0 | xargs -0 stat --format "%i %n %Z" | sort -t ' ' -k 3 -nr | head -n +1 | cut -d ' ' -f1)"
 
 while read line; do
